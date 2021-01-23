3ec77e9e49c7d3dfcefcb5685f15f1ba0c984a6b
Uddesh-bagade <uddesh.bagade@appxbuild.com>
2021-01-23 20:10:25 +0530

    till practice2.js

---
+++ javascript/jaascript practice/js/index.js
@@ -0,0 +1,8 @@
+console.time('Time taken');
+console.log('Good Morning');
+console.log([12,58,75,84]);
+console.log(7*8*8*654+7-66);
+console.log({uddesh:20,nilesh:19});
+console.table({uddesh:20,nilesh:19});
+console.warn('This is warning');
+console.timeEnd('Time taken');
---
+++ javascript/jaascript practice/js/practice2.js
@@ -0,0 +1,20 @@
+console.log('Practice2');
+
+// var, let, const
+
+var name = 'Uddesh';
+var age = '21';
+var city = 'kamptee';
+console.log(name, age , city);
+
+var city = 'Nagpur';
+console.log(city);
+
+const contactPerson = "Uddesh Bagade";
+console.log(contactPerson);
+
+{let city = 'mumbai';
+city = 'delhi';
+console.log(city);
+}
+console.log(city);
